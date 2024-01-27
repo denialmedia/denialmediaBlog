24c923df64e6a4daad9078754050fc5b82bb6cb9
Jason Kingery <denialmedia@gmail.com>
2024-01-27 16:41:42 -0600

    Added Pages, checking images.

--- app.py
+++ app.py
@@ -1,28 +1,50 @@
 from flask import Flask, render_template
 import markdown2
 import os
+from flask import Blueprint
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    # Get the directory containing the markdown files
-    content_dir = os.path.join(os.path.dirname(__file__), 'content')
-    markdown_files = os.listdir(content_dir)
+blueprint = Blueprint(
+    'md',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
 
-    # Load and convert content from all Markdown files
+def load_markdown_files(directory):
+    """
+    Load and convert content from all Markdown files in a given directory.
+    Args:
+    - directory: Path to the directory containing Markdown files.
+    Returns:
+    - html_content: Concatenated HTML content of all Markdown files.
+    """
+    markdown_files = os.listdir(directory)
     html_content = ""
     for file in markdown_files:
-        if file.endswith('.md'):  # Corrected file extension check
-            md_file_path = os.path.join(content_dir, file)
+        if file.endswith('.md'):  # Check for .md file extension
+            md_file_path = os.path.join(directory, file)
             with open(md_file_path, 'r') as md_file:
                 content = md_file.read()
-
             # Convert Markdown to HTML using markdown2 with extras
             html = markdown2.markdown(content, extras=["fenced-code-blocks"])
             html_content += html + "\n"
+    return html_content
 
+@app.route('/')
+def index():
+    # Get the directory containing the markdown files for the index page
+    content_dir = os.path.join(os.path.dirname(__file__), 'content')
+    html_content = load_markdown_files(content_dir)
     return render_template('index.html', content=html_content)
+
+@app.route('/projects')
+def projects():
+    # Get the directory containing the markdown files for the projects page
+    content_dir = os.path.join(os.path.dirname(__file__), 'projects')
+    html_content = load_markdown_files(content_dir)
+    return render_template('projects.html', content=html_content)
 
 if __name__ == '__main__':
     # Bind the app to 0.0.0.0 and the specified port for Render
---
+++ projects/main.md
@@ -0,0 +1,8 @@
+# Work
+___
+
+### Just testing images are working. 
+
+![Alt Text](/static/16color_ansi.png)
+
+*They seem to be*
